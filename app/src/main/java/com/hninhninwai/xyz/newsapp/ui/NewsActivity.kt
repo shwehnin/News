52d3d48ab84d210b2084398840827728f605417c
shwehnin <hninhnin6398@gmail.com>
2025-10-05 23:36:31 +0700

    view model and provider for news

--- app/src/main/java/com/hninhninwai/xyz/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/hninhninwai/xyz/newsapp/ui/NewsActivity.kt
@@ -5,17 +5,37 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.hninhninwai.xyz.newsapp.R
+import com.hninhninwai.xyz.newsapp.databinding.ActivityNewsBinding
+import com.hninhninwai.xyz.newsapp.db.ArticleDatabase
+import com.hninhninwai.xyz.newsapp.repository.NewsRepository
 
 class NewsActivity : AppCompatActivity() {
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var binding: ActivityNewsBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityNewsBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_news)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val newsViewModelProviderFactory = NewsViewModelProviderFactory(application, newsRepository)
+        newsViewModel =
+            ViewModelProvider(this, newsViewModelProviderFactory).get(NewsViewModel::class.java)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.newsNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationBarView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/hninhninwai/xyz/newsapp/ui/NewsViewModel.kt
+++ app/src/main/java/com/hninhninwai/xyz/newsapp/ui/NewsViewModel.kt
@@ -12,6 +12,7 @@ import com.hninhninwai.xyz.newsapp.models.NewsResponse
 import com.hninhninwai.xyz.newsapp.repository.NewsRepository
 import com.hninhninwai.xyz.newsapp.utils.Resource
 import kotlinx.coroutines.launch
+import okio.IOException
 import retrofit2.Response
 
 class NewsViewModel(app: Application, val newsRepository: NewsRepository) : AndroidViewModel(app) {
@@ -24,6 +25,18 @@ class NewsViewModel(app: Application, val newsRepository: NewsRepository) : Andr
     var searchNewsResponse: NewsResponse? = null
     var newSearchQuery: String? = null
     var oldSearchQuery: String? = null
+
+    init {
+        getHeadlines("us")
+    }
+
+    fun getHeadlines(countryCode: String) = viewModelScope.launch {
+        headlinesInternet(countryCode)
+    }
+
+    fun searchNews(searchQuery: String) = viewModelScope.launch {
+        searchNewsInternet(searchQuery)
+    }
 
     private fun handleHeadlinesResponse(response: Response<NewsResponse>): Resource<NewsResponse> {
         if (response.isSuccessful) {
@@ -85,5 +98,38 @@ class NewsViewModel(app: Application, val newsRepository: NewsRepository) : Andr
         }
     }
 
+    private suspend fun headlinesInternet(countryCode: String) {
+        headlines.postValue(Resource.Loading())
+        try {
+            if(internetConnection(this.getApplication())) {
+                val response = newsRepository.getHeadlines(countryCode, headlinesPage)
+                headlines.postValue(handleHeadlinesResponse(response))
+            }else {
+                headlines.postValue(Resource.Error("No Internet Connection"))
+            }
+        }catch (t: Throwable) {
+            when(t) {
+                is IOException -> headlines.postValue(Resource.Error("Unable to connect"))
+                else -> headlines.postValue(Resource.Error("No Signal"))
+            }
+        }
+    }
 
+    private suspend fun searchNewsInternet(searchQuery: String) {
+        newSearchQuery = searchQuery
+        searchNews.postValue(Resource.Loading())
+        try {
+            if(internetConnection(this.getApplication())) {
+                val response = newsRepository.searchNews(searchQuery, searchNewsPage)
+                searchNews.postValue(handleSearchNewsResponse(response))
+            }else {
+                searchNews.postValue(Resource.Error("No Internet Connection"))
+            }
+        }catch (t: Throwable) {
+            when(t) {
+                is IOException -> searchNews.postValue(Resource.Error("Unable to connect"))
+                else -> searchNews.postValue(Resource.Error("No Signal"))
+            }
+        }
+    }
 }
