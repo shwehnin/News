b03543758beecbdd30c8132deca9b1dbc8b962c5
shwehnin <hninhnin6398@gmail.com>
2025-10-06 16:15:22 +0700

    add headlines and article

--- app/src/main/java/com/hninhninwai/xyz/newsapp/ui/fragments/HeadlinesFragment.kt
+++ app/src/main/java/com/hninhninwai/xyz/newsapp/ui/fragments/HeadlinesFragment.kt
@@ -1,18 +1,153 @@
 package com.hninhninwai.xyz.newsapp.ui.fragments
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AbsListView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.button.MaterialButton
 import com.hninhninwai.xyz.newsapp.R
+import com.hninhninwai.xyz.newsapp.adapters.NewsAdapter
+import com.hninhninwai.xyz.newsapp.databinding.FragmentHeadlinesBinding
+import com.hninhninwai.xyz.newsapp.ui.NewsActivity
+import com.hninhninwai.xyz.newsapp.ui.NewsViewModel
+import com.hninhninwai.xyz.newsapp.utils.Constants
+import com.hninhninwai.xyz.newsapp.utils.Resource
 
 class HeadlinesFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_headlines, container, false)
+    lateinit var newsViewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapter
+    lateinit var retryBtn: MaterialButton
+    lateinit var errorText: TextView
+    lateinit var itemHeadlinesError: CardView
+    lateinit var binding: FragmentHeadlinesBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHeadlinesBinding.bind(view)
+
+        itemHeadlinesError = view.findViewById(R.id.itemHeadlinesError)
+        val inflater =
+            requireContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view: View = inflater.inflate(R.layout.item_error, null)
+
+        retryBtn = view.findViewById(R.id.retryBtn)
+        errorText = view.findViewById(R.id.errorText)
+
+        newsViewModel = (activity as NewsActivity).newsViewModel
+        setupHeadlinesRecycler()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(R.id.action_headlinesFragment_to_articleFragment, bundle)
+        }
+
+        newsViewModel.headlines.observe(viewLifecycleOwner, Observer{response ->
+            when(response) {
+                is Resource.Success<*> -> {
+                    hideProgressBar()
+                    hideErrorMessage()
+                    response.data?.let { newsResponse ->
+                        newsAdapter.differ.submitList(newsResponse.articles.toList())
+                        val totalPages = newsResponse.totalResults / Constants.QUERY_PAGE_SIZE +2
+                        isLastPage = newsViewModel.headlinesPage == totalPages
+                        if(isLastPage) {
+                            binding.headlinesRecycler.setPadding(0, 0, 0, 0)
+                        }
+                    }
+                }
+                is Resource.Error<*> -> {
+                    hideProgressBar()
+                    response.message?.let { message ->
+                        Toast.makeText(activity, "Sorry error : $message", Toast.LENGTH_SHORT).show()
+                        showErrorMessage(message)
+                    }
+                }
+
+                is Resource.Loading<*> -> {
+                    showProgressBar()
+                }
+            }
+        })
+
+        retryBtn.setOnClickListener {
+            newsViewModel.getHeadlines("us")
+        }
+    }
+
+    var isError = false
+    var isLoading = false
+    var isLastPage = false
+    var isScrolling = false
+
+    private fun hideProgressBar() {
+        binding.paginationProgressBar.visibility = View.INVISIBLE
+        isLoading = false
+    }
+
+    private fun showProgressBar() {
+        binding.paginationProgressBar.visibility = View.VISIBLE
+        isLoading = true
+    }
+
+    private fun hideErrorMessage() {
+        itemHeadlinesError.visibility = View.INVISIBLE
+        isLoading = false
+    }
+
+    private fun showErrorMessage(message: String) {
+        itemHeadlinesError.visibility = View.VISIBLE
+        errorText.text = message
+        isLoading = true
+    }
+
+    val scrollListener = object : RecyclerView.OnScrollListener() {
+        override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+            super.onScrolled(recyclerView, dx, dy)
+
+            val layoutManager = recyclerView.layoutManager as LinearLayoutManager
+            val firstVisibleItemPosition = layoutManager.findFirstVisibleItemPosition()
+            val visibleItemCount = layoutManager.childCount
+            val totalItemCount = layoutManager.itemCount
+            val isNoErrors = !isError
+            val isNotLoadingAndLastPage = !isLoading && !isLastPage
+            val isAtLastItem = firstVisibleItemPosition + visibleItemCount >= totalItemCount
+            val isNotAtBeginning = firstVisibleItemPosition >= 0
+            val isTotalMoreThanVisible = totalItemCount >= Constants.QUERY_PAGE_SIZE
+            val shouldPaginate =
+                isNoErrors && isNotLoadingAndLastPage && isAtLastItem && isNotAtBeginning && isTotalMoreThanVisible && isScrolling
+            if (shouldPaginate) {
+                newsViewModel.getHeadlines("us")
+                isScrolling = false
+            }
+        }
+
+        override fun onScrollStateChanged(recyclerView: RecyclerView, newState: Int) {
+            super.onScrollStateChanged(recyclerView, newState)
+            if (newState == AbsListView.OnScrollListener.SCROLL_STATE_TOUCH_SCROLL) {
+                isScrolling = true
+            }
+        }
+    }
+
+    private fun setupHeadlinesRecycler() {
+        newsAdapter = NewsAdapter()
+        binding.headlinesRecycler.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+            addOnScrollListener(this@HeadlinesFragment.scrollListener)
+        }
     }
 }
--- app/src/main/java/com/hninhninwai/xyz/newsapp/utils/Constants.kt
+++ app/src/main/java/com/hninhninwai/xyz/newsapp/utils/Constants.kt
@@ -4,7 +4,7 @@ class Constants {
     companion object {
         const val API_KEY = "f308278bbefc4bb7855f124b4e36c8f4"
         const val BASE_URL = "https://newsapi.org/"
-        const val SEARCH_NEWS_TIME_DELAY = "500L"
-        const val QUERY_PAGE_SIZE = "20"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
